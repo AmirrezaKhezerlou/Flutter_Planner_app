6dbdfdd568dc6f2d371d5fcf86ff0c2cdd7992d9
Khezerlouamirreza@gmail.com <mrkhezerlou@gmail.com>
2024-01-28 22:06:54 +0330

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:planner/home/Views/home.dart';
 
-void main() async{
+void main() async{ 
   WidgetsFlutterBinding.ensureInitialized();
 await Alarm.init();
   runApp(const MyApp());
